e393e68f9fe9e210fe4b0d478fdc3708af43d923
Ahtisham Ahmed <jackport530@gmail.com>
2022-02-10 16:42:48 +0500

    Pass code View updated

--- app/src/main/java/com/example/testing/Password.java
+++ app/src/main/java/com/example/testing/Password.java
@@ -6,11 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.hanks.passcodeview.PasscodeView;
 
 public class Password extends AppCompatActivity {
     PasscodeView passcode;
+    EditText passen;
+    Button pasv;
     String thepasscode;
     SharedPreferences sharedPreferences;
 
@@ -24,6 +29,10 @@ public class Password extends AppCompatActivity {
         setContentView(R.layout.activity_password);
         getSupportActionBar().hide();
 
+        passen=findViewById(R.id.pass);
+        pasv = findViewById(R.id.passvery);
+
+
 
         sharedPreferences  = getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
         String pascode =sharedPreferences.getString(KEY_PASS,null);
@@ -33,22 +42,29 @@ public class Password extends AppCompatActivity {
             thepasscode=pascode;
         }
 
-            passcode = findViewById(R.id.passcodeView);
-            passcode.setPasscodeLength(4).setLocalPasscode(thepasscode)
-                    .setListener(new PasscodeView.PasscodeViewListener() {
-                        public void onFail() {
-                            //if password is wrong
-                        }
+        pasv.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String pas=passen.getText().toString();
 
-                        @Override
-                        public void onSuccess(String number) {
-                            String rue = "tru";
+                if(thepasscode.equals(pas))
+                {
+                    Intent in = new Intent(Password.this, MainActivity.class);
+                    startActivity(in);
 
-                            Intent in = new Intent(Password.this, MainActivity.class);
-                            startActivity(in);
+                }
+                else
+                {
 
-                        }
-                    });
+                    passen.setError("Wrong PassCode");
+                    passen.setText("");
+
+                }
+            }
+        });
+
 
 
         }
